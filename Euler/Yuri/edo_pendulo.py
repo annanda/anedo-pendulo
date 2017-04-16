7de0d8772ae6349dd83163d53bdead6e39bbe068
Annanda Sousa <annanda.sousa@gmail.com>
2017-04-16 20:22:27 -0300

    Arrumando os argumentos dos métodos e PEP8.

--- Euler/Yuri/edo_pendulo.py
+++ Euler/Yuri/edo_pendulo.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import sys
 
 
 class EDOPendulo(object):
-    def __init__(self, g, L, t_inicial, t_final, n_intervalos):
+    def __init__(self, t_inicial, t_final, n_intervalos, v_inicial, theta_inicial=np.pi/24,  g=10, L=1):
         self.g = g
+        self.theta_inicial = theta_inicial
         self.L = L
         self.t_inicial = t_inicial
         self.t_final = t_final
+        self.v_inicial = v_inicial
         self.n_intervalos = n_intervalos
         self.atualizar_h()
         
     def atualizar_h(self):
         self.h = (self.t_final - self.t_inicial)/float(self.n_intervalos)        
         
-    def euler_explicito_nao_linear(self, theta0, v0):
-        theta = [theta0]
-        v = [v0]
+    def euler_explicito_nao_linear(self):
+        theta = [self.theta_inicial]
+        v = [self.v_inicial]
         t = [self.t_inicial]
         
         for i in xrange(0, self.n_intervalos):
-            theta_next = theta[i]+self.h*v[i]
-            v_next = v[i] - self.h*((self.g/self.L)*np.sin(theta_next))
+            theta_next = theta[i]+self.h * v[i]
+            v_next = v[i] - self.h * ((self.g/self.L) * np.sin(theta_next))
             t_next = t[i] + self.h
             
             theta.append(theta_next)
@@ -31,15 +34,15 @@ class EDOPendulo(object):
         
         return np.array(t), np.array(theta), np.array(v)
     
-    def euler_implicito_linear(self, theta0, v0):
-        theta = [theta0]
-        v = [v0]
+    def euler_implicito_linear(self):
+        theta = [self.theta_inicial]
+        v = [self.v_inicial]
         t = [self.t_inicial]
         
         for i in xrange(0, self.n_intervalos):
             
-            v_next = (v[i] - (self.h*(self.g/self.L)*theta[i])) / (1. + (self.h**2)*self.g/self.L)
-            theta_next = theta[i] + self.h*v_next
+            v_next = (v[i] - (self.h * (self.g/self.L) * theta[i])) / (1. + (self.h**2) * self.g/self.L)
+            theta_next = theta[i] + self.h * v_next
             t_next = t[i] + self.h
             
             theta.append(theta_next)
@@ -48,15 +51,15 @@ class EDOPendulo(object):
         
         return np.array(t), np.array(theta), np.array(v)
     
-    def euler_implicito_nao_linear(self, theta0, v0):
-        theta = [theta0]
-        v = [v0]
+    def euler_implicito_nao_linear(self):
+        theta = [self.theta_inicial]
+        v = [self.v_inicial]
         t = [self.t_inicial]
         
         for i in xrange(0, self.n_intervalos):
             
             v_next = self.newton(theta[i], v[i])
-            theta_next = theta[i] + self.h*v_next
+            theta_next = theta[i] + self.h * v_next
             t_next = t[i] + self.h
             
             theta.append(theta_next)
@@ -66,41 +69,41 @@ class EDOPendulo(object):
         return np.array(t), np.array(theta), np.array(v)
     
     def f(self, v_next, theta, v):
-        return v_next - (v - (self.h*(self.g/self.L))*np.sin(theta + self.h*v_next))
+        return v_next - (v - (self.h * (self.g/self.L)) * np.sin(theta + self.h * v_next))
     
     def df(self, v_next, theta, v):
-        return self.g*(self.h**2)*np.cos(self.h*v_next + theta)/self.L + 1.
+        return self.g * (self.h**2) * np.cos(self.h * v_next + theta)/self.L + 1.
     
     def newton(self, theta, v):
         x = 10. #initial guess
         
         i=0
         while (i<100): #100 iterations
-            x_next = x-self.f(x, theta, v)/self.df(x, theta, v)            
+            x_next = x-self.f(x, theta, v) / self.df(x, theta, v)
             x = x_next
             i+=1        
         
         return x
 
+if __name__ == '__main__':
+    theta = np.pi/24
+    v = 0.
+    g = 10
+    L = 1
+    t_inicial = 0.
+    t_final = 10.
+    n_intervalos = 100
 
-theta = np.pi/24
-v = 0.
-g = 10
-L = 1
-t_inicial = 0.
-t_final = 10.
-n_intervalos = 100
+    edo = EDOPendulo(t_inicial, t_final, n_intervalos, v, theta, g, L)
+    t_explNL, p_explNL, v_explNL = edo.euler_implicito_linear()
+    t_implL, p_implL, v_implL = edo.euler_implicito_linear()
+    t_implNL, p_implNL, v_implNL = edo.euler_implicito_nao_linear()
 
-edo = EDOPendulo(g, L, t_inicial, t_final, n_intervalos)
-t_explNL, p_explNL, v_explNL = edo.euler_implicito_linear(theta, v)
-t_implL, p_implL, v_implL = edo.euler_implicito_linear(theta, v)
-t_implNL, p_implNL, v_implNL = edo.euler_implicito_nao_linear(theta, v)
+    error = np.absolute(p_implNL - p_implL)
 
-error = np.absolute(p_implNL - p_implL)
+    print "Erro em theta: "
+    print (np.amax(error))
 
-print "Erro em theta: "
-print (np.amax(error))
-
-error = np.absolute(v_implNL - v_implL)
-print "Erro em v: "
-print (np.amax(error))
+    error = np.absolute(v_implNL - v_implL)
+    print "Erro em v: "
+    print (np.amax(error))
