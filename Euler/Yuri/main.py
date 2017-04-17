b7fae20560d82961490e9ca4f5eb49429caa73b3
Annanda Sousa <annanda.sousa@gmail.com>
2017-04-16 21:55:55 -0300

    Fazendo com que a saida seja um arquivo CSV com os vetores t, p e v.

--- Euler/Yuri/main.py
+++ Euler/Yuri/main.py
@@ -3,8 +3,17 @@ from edo_pendulo import *
 
 import click
 import numpy as np
+import csv
 
 METODOS_CHOICE = ['euler_implicito_linear', 'euler_implicito_nao_linear', 'euler_explicito_nao_linear']
+
+
+def write_csv(t, p, v):
+    with open('resultado.csv', 'wb') as file:
+        spamwriter = csv.writer(file, delimiter=',')
+        spamwriter.writerow(t)
+        spamwriter.writerow(p)
+        spamwriter.writerow(v)
 
 @click.command()
 @click.option('--metodo', type=click.Choice(METODOS_CHOICE), default=METODOS_CHOICE[0], help=u'Nome do método desejado')
@@ -27,9 +36,8 @@ def main(metodo, tempo_inicial, tempo_final, n_intervalos, v_inicial, theta_inic
     )
     if metodo == METODOS_CHOICE[0]:
         t, p, v = edo.euler_implicito_linear()
-        click.echo("t: {}\n".format(t))
-        click.echo("p: {}\n".format(p))
-        click.echo("v: {}\n".format(v))
+        write_csv(t, p, v)
+
 
 if __name__ == '__main__':
     main()
